3bc5c993c1706c56bd33a69010edce0d688d1a31
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-13 10:33:29 +0500

    custom instance, fix downside of global defaults (e.g. security issues)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Title from './components/Title';
-import Setup from './examples/4-global-instance'
-import './axios/global'
+import Setup from './examples/5-custom-instance'
+// import './axios/global'
 
 function App() {
   return (
--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,10 +1,18 @@
 import { useEffect } from 'react';
+import authFetch from '../axios/custom';
+import axios from 'axios';
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
     console.log('custom axios instance');
+    try {
+      await axios(randomUserUrl)
+      authFetch('/react-store-products')
+    } catch (error) {
+      console.log(error.response)
+    }
   };
 
   useEffect(() => {
