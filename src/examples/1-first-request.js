19a6863566c8398b9c85794e12f1ef09c7b4ad35
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-13 10:00:33 +0500

    axios GET request + response.data + error & error.response

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,10 +1,23 @@
+import axios from 'axios';
 import { useEffect } from 'react';
 // limit, if 429 wait for 15 min and try again
 const url = 'https://course-api.com/react-store-products';
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const resp = await axios.get(url)
+      console.log(resp)
+      console.log(resp.data)
+    } catch (error) {
+      console.log(error)
+      console.log(error.response)
+    }
+  }
+
   useEffect(() => {
     console.log('first axios request');
+    fetchData()
   }, []);
 
   return <h2 className='text-center'>first request</h2>;
